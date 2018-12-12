ef64801952dc18f349a9aa474b770570ee01d2aa
gilbert-z <274138089@qq.com>
2018-12-12 16:15:29 +0800

    菜谱操作

---
+++ Easycook/src/main/java/com/eco/easycook/pojo/Material.java
@@ -0,0 +1,26 @@
+package com.eco.easycook.pojo;
+
+public class Material {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    private String content;
+
+
+}
